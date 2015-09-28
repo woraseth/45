9ca63e1b641b4f017ba8caf6d15a9c869d17b89c
woraseth <woraseth@users.noreply.github.com>
2015-09-29 04:35:47 +0700

    Create music.py

---
+++ music.py
@@ -0,0 +1,13 @@
+# music (note, octave) to freq
+
+a = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+d = dict(zip(a, range(len(a))))
+
+tt = int(input())
+for _ in range(tt):
+  line = input().split(' ')
+  note = line[0]
+  octave = int(line[1])
+  step = d[note] - d['A']
+  n = (octave - 4) * 12 + step
+  print('%s%d %.3f' % (note, octave, 440 * 2**(n/12)))
