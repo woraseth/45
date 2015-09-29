f2145b94191ae41f0cecce0b961f9b8a65419c4a
woraseth <woraseth@users.noreply.github.com>
2015-09-30 00:18:29 +0700

    Create salung.py

---
+++ salung.py
@@ -0,0 +1,9 @@
+target = 0
+cnt = 0
+for a in range(1 + target // 100):
+  for b in range(1 + target // 50):
+    for c in range(1 + target // 25):
+      if a*100 + b*50 + c*25 == target:
+        print(a,b,c)
+        cnt += 1
+print(cnt)
