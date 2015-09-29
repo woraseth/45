cb4b772dda976387fb7f5d6d7362e2f406dea401
woraseth <woraseth@users.noreply.github.com>
2015-09-29 23:16:39 +0700

    Create hdb_up.py

---
+++ hdb_up.py
@@ -0,0 +1,13 @@
+amas = [34,36,39,42,45,47,50,53,55]
+awan = [40,43,49,52]
+norm_day, awan_day, amas_day = 354, 355, 384
+days = 0
+for year in range(55, 33, -1):
+  from datetime import date, timedelta
+  print(date(2012,12,14) - timedelta(days=days+16))
+  if year in amas:
+    days += amas_day
+  elif year in awan:
+    days += awan_day
+  else:
+    days += norm_day
