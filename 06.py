00bd0a10dfca2a2a41f9819698aa8ea3281273c7
woraseth <woraseth@users.noreply.github.com>
2015-09-30 20:29:55 +0700

    Update 06.py

--- 06.py
+++ 06.py
@@ -1,11 +1,11 @@
 # 06 - quarter
 
-target = 0
+target = int(float(input())*100)
 cnt = 0
 for a in range(1 + target // 100):
   for b in range(1 + target // 50):
     for c in range(1 + target // 25):
       if a*100 + b*50 + c*25 == target:
-        print(a,b,c)
+        # print(a,b,c)
         cnt += 1
 print(cnt)
