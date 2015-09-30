0bdf94126aafe67afc41bcd0b309278f01d470ea
woraseth <woraseth@users.noreply.github.com>
2015-09-29 17:02:02 -0700

    Create 59.py

---
+++ 59.py
@@ -0,0 +1,16 @@
+freq = {}
+while True:
+  try:
+    s = input()
+    s = s[:s.rfind('\\')]
+    if s in freq:
+      freq[s] += 1
+    else:
+      freq[s] = 1
+  except:
+    break
+
+a = list(freq)
+a.sort()
+for s in a:
+  print(s, freq[s])
