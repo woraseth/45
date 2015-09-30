49f18eb0cb6212c06964c69acd64d7bd7ac30e6b
woraseth <woraseth@users.noreply.github.com>
2015-09-30 19:23:11 +0700

    Update 39.py

--- 39.py
+++ 39.py
@@ -1,12 +1,13 @@
 # 39 - intania 
 
+p = ['', 'k', 'M', 'G', 'T', 'P', 'E']
 def engineer(s):
   exp = len(s) - 1
   exp //= 3
   pos = len(s) - exp * 3
   s = s[:pos] + '.' + s[pos:]
   s = s.rstrip('0').rstrip('.')
-  return s + 'e%d' % (exp * 3)
+  return s + '%s' % (p[exp])
 
 tt = int(input())
 for _ in range(tt):
