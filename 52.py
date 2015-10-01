72a1e8f60e9b3883f1eaccf244c6a5d733e0bb76
woraseth <woraseth@users.noreply.github.com>
2015-09-30 19:13:50 -0700

    Create 52.py

---
+++ 52.py
@@ -0,0 +1,17 @@
+# 52 - tourbillon
+
+tt = int(input())
+for _ in range(tt):
+  line = input()
+  a = line.split(':')
+  hour = int(a[0])
+  if hour == 12:
+    hour = 0
+  minute = int(a[1])
+  minRatio = minute / 60;
+  hourDegree = (hour + minRatio) * 30;
+  minDegree = minute * 6;
+  diff = abs(hourDegree - minDegree);
+  if diff > 180:
+    diff = 360 - diff
+  print('%.3f' % diff)
