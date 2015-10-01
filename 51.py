89217e8822408773416bee02c1ae906b496652a8
woraseth <woraseth@users.noreply.github.com>
2015-09-30 19:39:31 -0700

    Create 51.py

---
+++ 51.py
@@ -0,0 +1,11 @@
+# 51 - pie chart
+
+tt = int(input())
+for _ in range(tt):
+  a = [int(s) for s in input().split()]
+  sm = sum(a)
+  deg = 0;
+  for i in range(len(a)-1):
+    deg += a[0] * 360 / sm
+    print('%.2f' % deg, end=' ')
+  print()
