7bc32f0194436a0e6f9b6e06c242da4bb9e42d89
woraseth <woraseth@users.noreply.github.com>
2015-09-30 21:05:51 -0700

    Update angle.py

--- angle.py
+++ angle.py
@@ -1,11 +1,11 @@
-# angle between two vector
+# angle between two vectors
 
 a = [-2, -1]
-b = [2, -1]
-
+#b = [2, -1]
+b = [0, 2]
 from math import *
 x = sum(a[i] * b[i] for i in range(len(a)))
-x /= sqrt(sum(a[i] * a[i] for i in range(len(a))))
-x /= sqrt(sum(b[i] * b[i] for i in range(len(a))))
+x /= sqrt(sum(i*i for i in a))
+x /= sqrt(sum(i*i for i in b))
 angle = acos(x)
 print(degrees(angle))
