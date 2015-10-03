9c6901551e75659fdbf725659b412701fa086d41
woraseth <woraseth@users.noreply.github.com>
2015-10-03 16:15:18 -0700

    Create 58.py

---
+++ 58.py
@@ -0,0 +1,14 @@
+# 58 - grade
+
+tt = int(input())
+grade = {'A':4, 'B+':3.5, 'B':3, 'C+':2.5, 'C':2, 'D+':1.5, 'D':1, 'F':0}
+sum_c = 0
+sum_w = 0
+for _ in range(tt):
+  a = input().split()
+  c = int(a[0])
+  g = grade[a[1]]
+  sum_c += c
+  sum_w += c * g
+print('%.2f' % (sum_w / sum_c))
+  
