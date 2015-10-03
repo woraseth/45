23666ca2e5daa8b829df98aca0063989e92f0804
woraseth <woraseth@users.noreply.github.com>
2015-10-03 09:04:51 +0700

    Update 34.py

--- 34.py
+++ 34.py
@@ -3,11 +3,11 @@
 tt = int(input())
 m = [[' '] * 70 for i in range(70)]
 for _ in range(tt):
-  xx, ww, hh = (int(s) for s in input().split())
-  xx -= 1
-  for h in range(hh):
-    for w in range(ww):
-      m[h][xx+w] = 'X'
+  x, width, height = (int(s) for s in input().split())
+  x -= 1
+  for h in range(height):
+    for w in range(width):
+      m[h][x+w] = 'X'
 for h in reversed(range(70)):
   if 'X' in m[h]:
     print(''.join(m[h]))
