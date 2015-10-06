73271d2901aa630df577d8766dca69f3560784ad
woraseth <woraseth@users.noreply.github.com>
2015-10-06 09:15:47 +0700

    Update 08.py

--- 08.py
+++ 08.py
@@ -6,4 +6,9 @@ tt = int(input())
 for _ in range(tt):
   c, d = (int(s) for s in input().split('/'))
   g = gcd(c, d)
-  print('%d/%d' % (c / g, d / g))
+  c //= g
+  d //= g
+  if c // d != 0:
+    print(c // d, end=' ')
+  if c % d != 0:
+    print('%d/%d' % (c % d, d))
