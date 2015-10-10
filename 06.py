a0f4c96640e9745a9998e8f4776fee7edaf4cd86
woraseth <woraseth@users.noreply.github.com>
2015-10-10 06:32:23 -0700

    Update 06.py

--- 06.py
+++ 06.py
@@ -1,14 +1,14 @@
 # 06 - grade
 
+cr = [int(s) for s in input().split()]
 tt = int(input())
 grade = {'A':4, 'B+':3.5, 'B':3, 'C+':2.5, 'C':2, 'D+':1.5, 'D':1, 'F':0}
-sum_c = 0
-sum_w = 0
 for _ in range(tt):
-  a = input().split()
-  c = int(a[0])
-  g = grade[a[1]]
-  sum_c += c
-  sum_w += c * g
-print('%.2f' % (sum_w / sum_c))
-  
+  sum_c = 0
+  sum_w = 0
+  for i in enumerate(input().split()):
+    c = cr[i[0]]
+    g = grade[i[1]]
+    sum_c += c
+    sum_w += c * g
+  print('%.2f' % (sum_w / sum_c))
