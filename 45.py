1edf1fa3696a3ae0717d5a50635bd0d53356b5f8
woraseth <woraseth@users.noreply.github.com>
2015-10-15 19:48:10 +0700

    Create 45.py

---
+++ 45.py
@@ -0,0 +1,11 @@
+# Jelly Wall
+
+def trapezoidal(f, a, b, n):
+  h = (b - a) / n
+  s = sum(f(a + i*h) for i in range(1, n))
+  return (f(a) + 2*s + f(b)) * h / 2
+
+tt = int(input())
+for _ in range(tt):
+  n = int(input())
+  print('%.1f' % trapezoidal(lambda x : -3*x*x + 12, -2, 2, n)) 
