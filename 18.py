cee0ea47dcd26813b41948ab5d75ae631a542c28
woraseth <woraseth@users.noreply.github.com>
2015-10-15 10:25:56 +0700

    Create 18.py

---
+++ 18.py
@@ -0,0 +1,17 @@
+def alphanum(c):
+  c = ord(c)
+  return (ord('a') <= c and c <= ord('z')) or \
+         (ord('0') <= c and c <= ord('9'))
+  
+words = input().split()
+tt = int(input())
+for _ in range(tt):
+  password = input()
+  lower = password.lower()
+  if len(password) < 10 or \
+     lower == password or \
+     any(w in lower for w in words) or \
+     all(alphanum(c) for c in lower):
+    print('weak password')
+  else:
+    print('strong password')
