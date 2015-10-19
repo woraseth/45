27cd7825f211e4cd349762316a852e2277631d28
woraseth <woraseth@users.noreply.github.com>
2015-10-19 20:07:00 +0700

    Update 47.py

--- 47.py
+++ 47.py
@@ -1,8 +1,9 @@
 # Tetranacci
 
 def f(n):
-  a = [0] * (n + 1)
-  a[0], a[1], a[2], a[3] = (0,1,1,2)
+  a = [0,1,1,2]
+  if n > 3:
+    a.extend([0] * (n-3))
   for i in range(4, len(a)):
     a[i] = a[i-1]+a[i-2]+a[i-3]+a[i-4] 
   return a[n]
