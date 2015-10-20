a384f2b6309d1edc0effcb574cc4b6c1a1c0cf68
woraseth <woraseth@users.noreply.github.com>
2015-10-20 17:07:36 +0700

    Create 11.py

---
+++ 11.py
@@ -0,0 +1,20 @@
+def prime(n):
+  is_prime = [True] * n
+  is_prime[0] = False
+  is_prime[1] = False
+  for i in range(2, n):
+    if is_prime[i]:
+      for j in range(i+i, n, i):
+        is_prime[j] = False
+  return is_prime
+  	
+is_prime = prime(1000000)
+tt = int(input())
+for _ in range(tt):
+  n = int(input())
+  i = 0
+  while n != 0:
+    i += 1
+    if is_prime[i]:
+      n -= 1
+  print(i)
