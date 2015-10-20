755fefa99e04393c0907e9d262ecac5f9aba35e5
woraseth <woraseth@users.noreply.github.com>
2015-10-20 19:50:53 +0700

    Create 44.py

---
+++ 44.py
@@ -0,0 +1,20 @@
+# moore machine
+binstr = input()
+state = int(input())
+d = {}
+out = {}
+cur = None
+for _ in range(state):
+  a = input().split()
+  d[a[0]] = (a[1], a[2])
+  out[a[0]] = a[2]
+  if cur == None:
+    cur = a[0]
+
+print(d)
+s = cur
+for b in binstr:
+  b = int(b)
+  cur = d[cur][b]
+  s += cur
+print(s)
