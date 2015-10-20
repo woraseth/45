ee3a200f225bc5b02fc54ec13ba4e1c5e3bfcdb1
woraseth <woraseth@users.noreply.github.com>
2015-10-20 19:12:51 +0700

    Create 45.py

---
+++ 45.py
@@ -0,0 +1,17 @@
+def dec(c):
+  cc = ord(c)
+  if ord('a') <= cc and cc <= ord('z'):
+    cc += 3
+    if cc > ord('z'):
+      cc = cc - ord('z') + ord('a') - 1
+  elif ord('A') <= cc and cc <= ord('Z'):
+    cc += 3
+    if cc > ord('Z'):
+      cc = cc - ord('Z') + ord('A') - 1
+  return chr(cc)
+  
+cipher = input()
+plain = ''
+for c in cipher:
+  plain += dec(c)
+print(plain)
