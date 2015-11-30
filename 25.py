af15d4da74b2532c4146a136c1469caad7fa4ba5
woraseth <woraseth@users.noreply.github.com>
2015-11-30 16:04:37 +0700

    Update 25.py

--- 25.py
+++ 25.py
@@ -13,6 +13,6 @@ for _ in range(tt):
     for i in range(5):
       b[i] = deck[i]
     b.sort()
-    if b[0] < b[1] and b[1] < b[2] and b[2] < b[3] and b[3] < b[4]:
+    if b[0] + 1 == b[1] and b[1] + 1 == b[2] and b[2] + 1 == b[3] and b[3] + 1 == b[4]:
       count += 1
-  print('%.3f' % (count*100/run))
+  print('%.2f' % (count*100/run))
