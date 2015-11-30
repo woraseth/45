c1ca0d403d158f614313f53a2f87399c78d183b8
woraseth <woraseth@users.noreply.github.com>
2015-11-30 16:16:43 +0700

    Update 25.py

--- 25.py
+++ 25.py
@@ -9,10 +9,8 @@ for _ in range(tt):
   count = 0
   for i in range(run):
     shuffle(deck)
-    b = [0] * 5
-    for i in range(5):
-      b[i] = deck[i]
+    b = deck[:5]
     b.sort()
     if b[0] + 1 == b[1] and b[1] + 1 == b[2] and b[2] + 1 == b[3] and b[3] + 1 == b[4]:
       count += 1
-  print('%.2f' % (count*100/run))
+  print('%.0f' % (count*100/run))
